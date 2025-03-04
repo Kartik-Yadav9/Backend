77bddc88c9c5aef357af95c278b6de6f7658d24e
kartik <kartikyadav0999@gmail.com>
2025-03-05 02:32:46 +0530

    promises fileSystem

--- Exports/EsModule/FileSystem/fileEsM.js
+++ Exports/EsModule/FileSystem/fileEsM.js
@@ -1,1 +1,17 @@
 import fs from  'fs/promises'
+
+let b =fs.writeFile("kartik2.txt", "hi, it is promise")
+
+b.then(()=>console.log('done')
+).catch(err=>console.log(err)
+)
+
+
+// fs.writeFile() does not return any value when successful.
+// In your .then(res => console.log(res)), res will always be undefined.
+
+let a = fs.readFile('kartik2.txt', 'utf-8')  
+// 'utf-8', Node.js automatically converts it to a readable string.
+
+a.then(data=>console.log(data))
+.catch(err=>console.log(err))
